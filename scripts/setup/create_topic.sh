25e4329eb38a16a0533425331432c13c9464d029
Avinash Mandava <avinash.mandava@gmail.com>
2016-04-01 15:09:21 -0700

    Reorg

--- scripts/setup/create_topic.sh
+++ scripts/setup/create_topic.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+#create the topic
 ~/confluent*/bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $1
--- scripts/setup/start_streaming.sh
+++ scripts/setup/start_streaming.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+#run the streaming code
 sudo ~/dse-*/bin/dse spark-submit --packages org.apache.spark:spark-streaming-kafka_2.10:1.4.1 analytics/writemetrics.py $1
